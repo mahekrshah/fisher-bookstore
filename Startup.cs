6843fda0269826d5ae71cb4016b3daa6ec960811
Mahek Shah <shah.941@osu.edu>
2019-02-13 18:54:01 -0500

    Lab 4

--- Startup.cs
+++ Startup.cs
@@ -15,8 +15,8 @@ namespace Fisher.Bookstore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -25,10 +25,40 @@ namespace Fisher.Bookstore
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
             });
+              app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=About}/{action=Index}/{id?}"
+                );
+
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Books}/{action=Index}/{id?}"
+                );
+
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Authors}/{action=Index}/{id?}"
+                );
+
+            });
+
         }
     }
 }
